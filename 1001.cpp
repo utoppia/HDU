d6827ef5cfa776ab0c26daa50f487c066f46f70e
utoppia <utoppia@users.noreply.github.com>
2014-04-18 21:54:12 +0800

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,16 @@
+// hdu 1001
+// by utoppia
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int flag=0;
+	long long n;
+	while(cin>>n)
+	{
+		cout<<n*(n+1)/2<<endl<<endl;
+		flag=1;
+	}	
+	return 0;
+}
