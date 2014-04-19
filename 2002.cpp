c824db6fc8260a6bb0fd5483f90d1ffc6c458897
utoppia <utoppia@users.noreply.github.com>
2014-04-19 10:23:33 +0800

    Create 2002.cpp

---
+++ 2002.cpp
@@ -0,0 +1,11 @@
+// hdu 2002
+// by utoppia
+
+#include<cstdio>
+double x;
+int main() {
+    while(scanf("%lf",&x)!=EOF){
+        printf("%.3lf\n",3.1415927*4.0/3.0*x*x*x);
+    }
+    return 0;
+}
